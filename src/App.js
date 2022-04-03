db811fa2d603e0c577438c23f5d4222f9f1af6b6
hossain101199 <hossainbhai1011@gmail.com>
2022-04-04 01:00:05 +0600

    project started.and folder has been created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
